9106d0697a7c51acebdbfb64a05e5840cfb74815
Hendra manase sitorus <72810939+Hendra89ms@users.noreply.github.com>
2020-11-28 09:24:57 +0700

    Create SortArray_menaikkan.cpp

---
+++ SortArray_menaikkan.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int array[] = {10, 35, 85, 93, 62, 77, 345, 43, 2, 10};
+int n = sizeof(array)/sizeof(array[0]);
+// 'sizeof 'memberikan ukuran total array yaitu ukuran setiap karakter * no. karakter
+// jadi untuk mendapatkan no. karakter
+// kita membagi sizeof (array) dengan ukuran salah satu karakter dari array
+// ini dia array [0]
+sort(array, array+n);
+cout << "nArray setelah menyortir menggunakan urutan default"<<endl;
+cout<<"adalah n : ";
+for (int i = 0; i < n; ++i)
+cout << array[i] << " ";
+return 0;
+}
