337561454ea0c2a7815f13a316be1152f82b9a43
Hendra manase sitorus <72810939+Hendra89ms@users.noreply.github.com>
2020-11-28 09:30:39 +0700

    Update SortArray_menaikkan.cpp

--- SortArray_menaikkan.cpp
+++ SortArray_menaikkan.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+Cout<<"Program SortArray Menaikkan"<<endl;
 int array[] = {10, 35, 85, 93, 62, 77, 345, 43, 2, 10};
 int n = sizeof(array)/sizeof(array[0]);
 // 'sizeof 'memberikan ukuran total array yaitu ukuran setiap karakter * no. karakter
